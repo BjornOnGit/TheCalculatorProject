4514a26eff8799dc0415fedae369b6f51d2669ad
Francis <fhrancorey99@gmail.com>
2022-06-23 17:39:52 +0100

    A new commit for this project

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is write a program that computes as a calculator the 4 basic arithmetic operations
---
+++ calculator.c
@@ -0,0 +1,38 @@
+/* This program is a simple calculator that computes all four basic arithmetic operations */
+#include<stdio.h>
+/* Starting off with creating functions for addition, subtraction, multiplication and division */
+
+double numAdd(double x, double y) /* function for addition */
+{
+    double result;
+    result = x + y;
+    return result;
+}
+
+double numSubtract(double x, double y) /* function for subtraction */
+{
+    double result;
+    result = x - y;
+    return result;
+}
+
+double numMulitiply(double x, double y) /* function for multiplication */
+{
+    double result;
+    result = x * y;
+    return result;
+}
+
+double numDivide(double x, double y) /* function for division */
+{
+    double result;
+    result = x / y;
+    return result;
+}
+
+/* the main body of the program */
+int main()
+{
+    
+    return 0;
+}
