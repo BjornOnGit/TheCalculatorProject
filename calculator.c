c33856c2a4b5d3fdb04e4bf7b0670d2dfd5112dc
Francis <fhrancorey99@gmail.com>
2022-06-27 09:30:51 +0100

    This commit resolves issues regarding precision modifiers and completes othercases in the switch statement

--- calculator.c
+++ calculator.c
@@ -9,34 +9,37 @@ double numAdd(double num1, double num2) /* function for addition */
     return result;
 }
 
-double numSubtract(double x, double y) /* function for subtraction */
+double numSubtract(double num1, double num2) /* function for subtraction */
 {
     double result;
-    result = x - y;
+    result = num1 - num2;
     return result;
 }
 
-double numMulitiply(double x, double y) /* function for multiplication */
+double numMulitiply(double num1, double num2) /* function for multiplication */
 {
     double result;
-    result = x * y;
+    result = num1 * num2;
     return result;
 }
 
-double numDivide(double x, double y) /* function for division */
+double numDivide(double num1, double num2) /* function for division */
 {
     double result;
-    result = x / y;
+    result = num1 / num2;
     return result;
 }
 
 /* the main body of the program */
 int main()
 {
-   int userSelect;
-   double num1;
-   double num2;
+   int userSelect; /* The variable for the user selection */
+   double num1; /* variable for the numbers to be accepted by the input */
+   double num2; // Same as above
    double sum;
+   double difference;
+   double product;
+   double quotient;
 
    printf("Welcome To The Simple Calculator!\n");
    printf("Please select an operation by entering a single digit\n");
@@ -47,15 +50,41 @@ int main()
    {
    case '1':     
        printf("Enter first number: \n");
-       scanf("%f", &num1);
+       scanf("%lf", &num1);
        printf("Enter second number: \n");
-       scanf("%f", &num2);
+       scanf("%lf", &num2);
        sum = numAdd(num1, num2);
-       printf("The answer is: %f\n", sum);
+       printf("The answer is: %lf\n", sum);
        break;
     
-   
+   case '2':
+       printf("Enter first number: \n");
+       scanf("%lf", &num1);
+       printf("Enter second number: \n");
+       scanf("%lf", &num2);
+       difference = numSubtract(num1, num2);
+       printf("The answer is: %lf\n", difference);
+       break;
+
+    case '3':
+       printf("Enter first number: \n");
+       scanf("%lf", &num1);
+       printf("Enter second number: \n");
+       scanf("%lf", &num2);
+       product = numMulitiply(num1, num2);
+       printf("The answer is: %lf\n", product);
+       break;
+
+    case '4':
+       printf("Enter first number: \n");
+       scanf("%lf", &num1);
+       printf("Enter second number: \n");
+       scanf("%lf", &num2);
+       quotient = numDivide(num1, num2);
+       printf("The answer is: %lf\n", quotient);
+       break;
    default:
+        printf("You chose an invalid operation. Try again.\n");
        break;
    } 
     return 0;
